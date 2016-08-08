138353e2742e86749d3e727e29805e0f00cbe89f
Flatiron School <github email address>
2016-08-08 07:31:27 -0400

    working cli more fixing needed

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,6 +6,7 @@ require_relative 'scraper.rb'
 
 class Cli
 
+  BASE_URL = "http://store.nike.com/us/en_us/pw/womens-lifestyle-shoes/7ptZoneZoi3?ipp=120"
   def run
     menu
   end
@@ -13,12 +14,18 @@ class Cli
 
   def menu
     puts "Welcome to CLI Nike"
+
     puts "We have 10 shoes for you to pick from"
     puts
-    Scraper.new.list
-    # list
 
+    count = 0
+    Scraper.new.shoe_names_and_prices(BASE_URL).each do |el|
+      puts "#{count += 1} #{el[:name]}  #{el[:price]}"
+    end
+
+    # Scraper.new(BASE_URL).list
   end
+
 
 
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,51 +5,42 @@ require 'pry'
 
 class Scraper
 
-    attr_accessor :parse_page
+    # attr_accessor :parse_page
 
     # BASE_URL = "http://store.nike.com/us/en_us/pw/womens-lifestyle-shoes/7ptZoneZoi3?ipp=120"
 
-    def initialize
-      doc = Nokogiri::HTML(open("http://store.nike.com/us/en_us/pw/womens-lifestyle-shoes/7ptZoneZoi3?ipp=120"))
-      # doc = Nokogiri::HTML(open(url))
-      # binding.pry
-    end
+    # def initialize
+    #   doc = Nokogiri::HTML(open("http://store.nike.com/us/en_us/pw/womens-lifestyle-shoes/7ptZoneZoi3?ipp=120"))
+    #
+    # end
 
-    def scrape_names
-      doc = Nokogiri::HTML(open("http://store.nike.com/us/en_us/pw/womens-lifestyle-shoes/7ptZoneZoi3?ipp=120"))
 
+    def scrape_names(url)
+      # doc = Nokogiri::HTML(open("http://store.nike.com/us/en_us/pw/womens-lifestyle-shoes/7ptZoneZoi3?ipp=120"))
+      doc = Nokogiri::HTML(open(url))
       name_list = doc.css(".product-name p").children.map{ |name| name.text }.compact
       no_repeats = []
       name_list.each_with_index { |name, i|  no_repeats << name if i.even? }
       twenty_names = no_repeats[1..10]
     end
 
-    def scrape_prices
-      doc = Nokogiri::HTML(open("http://store.nike.com/us/en_us/pw/womens-lifestyle-shoes/7ptZoneZoi3?ipp=120"))
-
+    def scrape_prices(url)
+      # doc = Nokogiri::HTML(open("http://store.nike.com/us/en_us/pw/womens-lifestyle-shoes/7ptZoneZoi3?ipp=120"))
+      doc = Nokogiri::HTML(open(url))
       prices = doc.css(".product-price span.local").children.map{ |price| price.text }.compact
 
       twenty_prices = prices[1..10]
     end
 
-    def shoe_names_and_prices
+    def shoe_names_and_prices(url)
 
-      # pair_array = []
-      #
-      #     scrape_names.each_with_index do |name, i|
-      #       pair_array << [name, scrape_prices[i]]
-      #
-      #       # binding.pry
-      #     end
-      #
-      # pair_array
       shoes = []
-      scrape_names.each_with_index do |name, i|
+      scrape_names(url).each_with_index do |name, i|
         shoe_hash = {}
         shoe_hash[:number] = i.to_i + 1
         shoe_hash[:name] = name
-        shoe_hash[:price] = scrape_prices[i.to_i]
-        binding.pry
+        shoe_hash[:price] = scrape_prices(url)[i.to_i]
+        # binding.pry
 
         shoes << shoe_hash
       end
@@ -58,8 +49,8 @@ class Scraper
       # binding.pry
     end
 
-    def list
-      shoe_names_and_prices.each do |shoe|
+    def list(url)
+      shoe_names_and_prices(url).each do |shoe|
 
         puts "#{shoe[:number]} #{shoe[:name]} #{shoe[:price]}"
       end
